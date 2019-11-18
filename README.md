469a97998112d2275ef416984d09f48b1802e6ab
donutwizard666 <thebeardedwarrior@icloud.com>
2019-11-18 13:48:01 -0800

    added in reducer switch cases

--- README.md
+++ README.md
@@ -1,14 +1,7 @@
 # front-end
-<<<<<<< HEAD
-
-## Product Canvas Link
-
-https://docs.google.com/document/d/14acT5nM-pM5Ac3ZHj_TlvmwQPfhYQZEpSI68izwFDc0/edit?pli=1#heading=h.4oicemcbd57
-=======
 - Project Canvas : https://docs.google.com/document/d/14acT5nM-pM5Ac3ZHj_TlvmwQPfhYQZEpSI68izwFDc0/edit?usp=sharing
 
 - Trello Board : https://trello.com/b/StzjR6nW/bookr
 
 - Airtable : https://airtable.com/shrtA1m4LFJAnjvqS/tblI02wuarVEYWVSv/viw2L09271lKsRt5x/rec28K3TrpH0YQe0q?blocks=hide
 
->>>>>>> 35161781450e9b8cd1dde7ac0cbee0f472126276
--- client/src/reducers/reducer.js
+++ client/src/reducers/reducer.js
@@ -1,11 +1,67 @@
-import {LOGIN_REQUEST} from '../actions';
+import {
+    LOGIN_REQUEST,
+    LOGIN_SUCCESS,
+    LOGIN_FAILURE,
+    REGISTER_REQUEST,
+    REGISTER_SUCCESS,
+    REGISTER_FAILURE,
+    LOGOUT,
+    LEAVE_BOOK,
+    FOCUS_BOOK,
+    DELETE_BOOK_REQUEST,
+    DELETE_BOOK_SUCCESS,
+    DELETE_BOOK_FAILURE,
+    SUBMIT_REVIEW_REQUEST,
+    SUBMIT_REVIEW_SUCCESS,
+    SUBMIT_REVIEW_FAILURE,
+} from '../actions';
+
+
 const initState = {
-    currentBook: null
+    token: null,
+    username: null,
+    currentBook: null,
+    error: null,
+    isAuthenticating: false,
+    isDeleting: false,
+    isReviewing: false,
+    books: [],
 }
 const reducer = (state=initState,action) => {
     switch(action.type){
         case LOGIN_REQUEST:
-            return state;
+        case REGISTER_REQUEST:
+            return { ...state, isAuthenticating: true, error: null};
+        case LOGIN_SUCCESS: 
+        case REGISTER_SUCCESS:
+            return { ...state, isAuthenticating: false, error: null, token: action.payload.token, username: action.payload.username};
+        case LOGIN_FAILURE: 
+        case REGISTER_FAILURE:
+            return { ...state, isAuthenticating: false, error: action.payload}
+        // case REGISTER_REQUEST:
+        //     return { ...state, isAuthenticating: true, error: null};
+        // case REGISTER_SUCCESS: 
+        //     return { ...state, isAuthenticating: false, error: null, token: action.payload.token, username: action.payload.username};
+        // case REGISTER_FAILURE: 
+        //     return { ...state, isAuthenticating: false, error: action.payload}
+        case LOGOUT: 
+            return { ...state, token: null, username: null, currentBook: null}
+        case LEAVE_BOOK: 
+            return { ...state, currentBook: null}
+        case FOCUS_BOOK:
+            return { ...state, currentBook: action.payload}
+        case DELETE_BOOK_REQUEST: 
+            return { ...state, isDeleting: true, error: null }
+        case DELETE_BOOK_SUCCESS: 
+            return { ...state, isDeleting: false, error: null}
+        case DELETE_BOOK_FAILURE: 
+            return { ...state, isDeleting: false, error: action.payload}
+        case SUBMIT_REVIEW_REQUEST:
+            return { ...state, isReviewing: true, error: null}
+        case SUBMIT_REVIEW_SUCCESS:
+            return { ...state, isReviewing: false, error: null}
+        case SUBMIT_REVIEW_FAILURE:
+            return { ...state, isReviewing: false, error: action.payload}
         default:
             return state;
     }
